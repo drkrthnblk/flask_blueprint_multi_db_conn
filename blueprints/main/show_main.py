e1f8ec9a7ab8c62635d7c384e451d1e299fbf313
Rajat <rarc111@gmail.com>
2021-02-11 04:36:28 -0800

    home page added

--- blueprints/main/show_main.py
+++ blueprints/main/show_main.py
@@ -4,6 +4,12 @@ import os
 main_bp = Blueprint('main', __name__, static_folder='static',static_url_path='/static',
                template_folder='templates')
 
+
+@main_bp.route('/')
+def show_all_links():
+    return render_template("show_links.html")
+
+
 @main_bp.route('/show_main')
 def show():
     return render_template("show_main.html")
